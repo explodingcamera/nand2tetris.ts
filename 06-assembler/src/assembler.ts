182a227fb54fb2456210b66215c468ac657072e3
Henry <mail@henrygressmann.de>
2020-09-24 16:32:06 +0200

    formatting
    Signed-off-by: Henry <mail@henrygressmann.de>

--- 06-assembler/src/assembler.ts
+++ 06-assembler/src/assembler.ts
@@ -1,17 +1,25 @@
-import {computation, jump, destination} from "./../data/translation.json"
+import { computation, jump, destination } from "./../data/translation.json";
 
-export const assemble = (operations: string[], symbols: {
-  [x: string]: number;
-}): string => {
-
-  // console.log(operations);
-  // console.log(symbols);
+export const assemble = (
+  operations: string[],
+  symbols: {
+    [x: string]: number;
+  }
+): string => {
 
   const ops = operations
-  .map(op => op.startsWith('@') && symbols.hasOwnProperty(op.slice(1)) ? '@'+(symbols[op.slice(1) as any]) : op)
-  .map(op => op.startsWith('(') && symbols.hasOwnProperty(op.slice(1)) ? '('+(symbols[op.slice(1) as any]) : op)
+    .map((op) =>
+      op.startsWith("@") && symbols.hasOwnProperty(op.slice(1))
+        ? "@" + symbols[op.slice(1) as any]
+        : op
+    )
+    .map((op) =>
+      op.startsWith("(") && symbols.hasOwnProperty(op.slice(1))
+        ? "(" + symbols[op.slice(1) as any]
+        : op
+    );
 
-  // console.log(ops)
+  console.log(ops)
 
-  return ''
-}  
+  return "";
+};
--- 06-assembler/src/parse.ts
+++ 06-assembler/src/parse.ts
@@ -1,22 +1,25 @@
-import buildIns from "./../data/buildIns.json"
+import buildIns from "./../data/buildIns.json";
 
 export const parseOperations = (file: string): string[] =>
-  file.replace(/(\/\/).*|\)|[^\S\r\n]/g, '')
-  .replace(/^\s*\n/gm, '')
-  .trim()
-  .split('\r')
+  file
+    .replace(/(\/\/).*|\)|[^\S\r\n]/g, "")
+    .replace(/^\s*\n/gm, "")
+    .trim()
+    .split("\r");
 
-export const parseSymbols = (operations: string[]): { [x: string]: number; } => 
-   ({
-    ...Object.fromEntries([...new Set(
-    operations
-    .filter(v => !Object.keys(buildIns).some(b => b === v.slice(1)))
-    .filter(v => v.startsWith('@'))
-    .map(v => v.slice(1))
-    // .filter(v => ["@", "("].some(s => s===v[0]))
-    .filter(v => Number.isNaN(parseInt(v)))
-    )].map(
-    (o, i) => [o, i+16]
-  )),
-  ...buildIns
-})
+export const parseSymbols = (
+  operations: string[]
+): { [x: string]: number } => ({
+  ...Object.fromEntries(
+    [
+      ...new Set(
+        operations
+          .filter((v) => !Object.keys(buildIns).some((b) => b === v.slice(1)))
+          .filter((v) => v.startsWith("@"))
+          .map((v) => v.slice(1))
+          .filter((v) => Number.isNaN(parseInt(v)))
+      ),
+    ].map((o, i) => [o, i + 16])
+  ),
+  ...buildIns,
+});
