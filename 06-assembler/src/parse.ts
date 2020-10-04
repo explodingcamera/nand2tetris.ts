df66369a200e43045b1005759847599bcbfc91fa
Henry <mail@henrygressmann.de>
2020-10-04 21:52:24 +0200

    refactor parser
    Signed-off-by: Henry <mail@henrygressmann.de>

--- 06-assembler/src/parse.ts
+++ 06-assembler/src/parse.ts
@@ -22,32 +22,23 @@ export const parseSymbols = (operations: string[]): Symbols => {
     {}
   );
 
-  let labelCount: number = 0
   let ops = operations
     .filter((v) => !Object.keys(buildIns).some((b) => b === v.slice(1)))
     .filter((v) => v.startsWith("@"))
     .map((v) => v.slice(1))
     .filter((v) => Number.isNaN(parseInt(v)))
     .filter((v, i, a) => a.indexOf(v) === i)
-    .map((o, i) => {
-
-      console.log(o, i + 16 - labelCount, labelCount);
-
-      if (!labels.hasOwnProperty(o)) {
-        return [o, i + 16 - labelCount]
-      }
-
-      if (!labels[o]) {
-        throw new Error("invalid label: " + o)
-      }
-
-      labelCount+=1;
-      return [o, labels[o] || (() => {})()]
-      // [o, labels.hasOwnProperty(o) ? labels[o] || (() => {throw new Error("invalid label: " + o)})() : i + 16])
-
-    });
-
-    
+    .reduce(
+      (result: string[][], v) => {
+        result[labels.hasOwnProperty(v) ? 0 : 1].push(v);
+        return result;
+      },
+      [[], []]
+    )
+    .map((x, isVar) =>
+      x.map((o, i) => (isVar ? [o, i + 16] : [o, labels[o] || (() => {})()]))
+    )
+    .flat();
 
   return {
     ...Object.fromEntries(ops),
